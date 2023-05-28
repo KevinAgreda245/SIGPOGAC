7e9fcd246c3057d6d07c9e34df873fb93f30d055
Kevin Agreda <36802620+KevinAgreda245@users.noreply.github.com>
2023-05-28 11:52:53 -0600

    Visualización de Información de Administradores

--- Administrador/urls.py
+++ Administrador/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.index, name = "Login"),
     path('main/', views.main, name = "Main"),
-    path('administrador/', views.management, name = "Administrador")
+    path('administrador/', views.management, name = "Administrador"),
+    path('administrador/<id>', views.details, name = "EditAdmin")
 ]
--- Administrador/views.py
+++ Administrador/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Usuario
 
 def index(request):
     return render(request, 'Administrador/index.html')    
@@ -7,4 +8,15 @@ def main(request):
     return render(request, 'Administrador/main.html')
 
 def management(request):
-    return render(request, 'Administrador/management.html')    
+    admins = Usuario.objects.filter(is_staff = 1)
+    context = {
+        "admins": admins
+    }
+    return render(request, 'Administrador/management.html',context)    
+
+def details(request,id):
+    admin = Usuario.objects.get(id = id)
+    context = {
+        "admin": admin
+    }
+    return render(request, 'Administrador/details.html',context)    
