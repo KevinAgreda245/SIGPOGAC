354f635a8e0f982f0e503d246f5bace213773aaf
marvinortiz22 <marvincubias22@gmail.com>
2023-05-31 16:39:48 -0600

    funcion ver cliente

--- Cliente/views.py
+++ Cliente/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from Cliente.models import Cliente
 
 def index(request):
-    return render(request, 'Cliente/index.html')
+    clientes=Cliente.objects.all()
+    return render(request, 'Cliente/index.html',{'clientes':clientes})
+
+def detailsCliente(request,id):
+    cliente=Cliente.objects.get(SK_CLIENTE=id)
+    return render(request,'Cliente/details.html',{"cliente":cliente})
