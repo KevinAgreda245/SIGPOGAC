3915794c3960d41648c23718a334d4f57c2ca325
Kevin Agreda <36802620+KevinAgreda245@users.noreply.github.com>
2023-06-01 21:14:01 -0600

    Campo para eliminación

--- Equipo/models.py
+++ Equipo/models.py
@@ -13,6 +13,7 @@ class Equipo(models.Model):
     SK_EQUIPO = models.AutoField(primary_key=True)
     ST_NOMBRE_EQUIPO = models.CharField(max_length=50,null=False,unique=True)
     ST_DESCRIPCION_EQUIPO = models.CharField(max_length=120,null=False)
+    BN_ESTADO_EQUIPO = models.BooleanField(null=False,default=1)
     SK_TIPO_EQUIPO = models.ForeignKey(TipoEquipo,models.CASCADE)
 
     class Meta:
--- Material/models.py
+++ Material/models.py
@@ -4,6 +4,7 @@ class Material(models.Model):
     SK_MATERIAL = models.AutoField(primary_key=True)
     ST_NOMBRE_MATERIAL = models.CharField(max_length=60,null=False,unique=True)
     ST_DESCRIPCION_MATERIAL = models.CharField(max_length=120,null=False)
+    BN_ESTADO_MATERIAL = models.BooleanField(null=False,default=1) 
 
     class Meta:
         db_table = "material"
