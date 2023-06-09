55da161446aadbf4dfa72e7e74a40f3a2d2b6069
GNavarroSV <gerardo.navarro2001@hotmail.com>
2023-06-09 16:59:10 -0600

    Actualización formulario cambio de contraseña

--- Seguridad/forms.py
+++ Seguridad/forms.py
@@ -27,7 +27,7 @@ class UserPasswordChangeForm(SetPasswordForm):
         super(UserPasswordChangeForm, self).__init__(*args, **kwargs)
         #self.fields["old_password"].widget = forms.PasswordInput(attrs={"class": "form-control"})
         self.fields["new_password1"].widget = forms.PasswordInput(attrs={"class": "form-control"})
-        self.fields["new_password2"].widget = forms.PasswordInput(attrs={"class": "form-control"})  
+        self.fields["new_password2"].widget = forms.PasswordInput(attrs={"class": "form-control"}) 
 
 
     
