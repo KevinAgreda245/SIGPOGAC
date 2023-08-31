5ed291cfa90e7c4d0b11f7a0d2af9d7f1017f767
MP20031 <kevin.portillo01@gmail.com>
2023-08-31 16:18:18 -0600

    agregando todos los formularios
    Agregando los form de los modelos de servicios

--- Proyecto/forms.py
+++ Proyecto/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from Proyecto.models import Proyecto,Transporte,Concreto
+from Proyecto.models import Proyecto,Concreto,Transporte,RentaEquipo,RentaDesimetro,LevantamientoTopografico,EstructuraMetalica,SenializacionVial,AsesoriaConstructiva
 from Cliente.models import Cliente
 
 
@@ -21,6 +21,35 @@ class ProyectoForm(forms.ModelForm):
         super(ProyectoForm, self).__init__(*args, **kwargs)
         self.fields['FK_CLIENTE'].queryset = Cliente.objects.filter(BN_ESTA_ACTIVO=True)
 
+class ConcretoForm(forms.ModelForm):
+    class Meta:
+        model = Concreto
+        fields = '__all__'
+        exclude = ['ST_DOC_CONCRETO', 'FK_PROYECTO']
+        widgets={
+          'ST_TIPO_DOC_CONCRETO': forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+          'ST_DOC_CONCRETO': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
+
+class RentaEquipoForm(forms.ModelForm):
+    class Meta:
+        model = RentaEquipo
+        fields = '__all__'
+        exclude = ['SK_RENTA_EQUIPO, FK_PROYECTO,FC_SALIDA_EQUIPO,FC_ENTRADA_EQUIPO']
+        widgets = {
+            'ST_TIPO_USO' : forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+            'ST_OBSERVACION_EQUIPO' : forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+        }
+
+class RentaDesimetroForm(forms.ModelForm):
+    class Meta:
+        model = RentaDesimetro
+        fields = '__all__'
+        exclude = ['SK_RENTA_DESIMETRO, FC_ENTRADA_DESIMETRO, FC_SALIDA_DESIMETRO, FK_PROYECTO']
+        widgets ={
+            'ST_NOMBRE_TECNICO' : forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+            'ST_OBSERVACION_DESIMETRO' : forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+        }
 
 class TransporteForm(forms.ModelForm):
     class Meta:
@@ -29,14 +58,46 @@ class TransporteForm(forms.ModelForm):
         exclude = ['NM_VOLUMEN,FK_PROYECTO']
         widgets = {
             'ST_UNIDAD_TRANSPORTE': forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+            'NM_VOLUMEN' : forms.Textarea(attrs={'class': 'form-control', 'rows' : '3'}),
         }
 
-class ConcretoForm(forms.ModelForm):
+
+class LevantamientoToporgraficoForm(forms.ModelForm):
     class Meta:
-        model = Concreto
+        model = LevantamientoTopografico
         fields = '__all__'
-        exclude = ['ST_DOC_CONCRETO', 'FK_PROYECTO']
-        widgets={
-          'ST_TIPO_DOC_CONCRETO': forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
-          'ST_DOC_CONCRETO': forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+        exclude = ['SK_LEVANTAMIENTO_TOPOGRAFICO, FK_PROYECTO']
+        widgets = {
+            'NM_AREA' : forms.Textarea(attrs={'class': 'form-control', 'rows' : '3'}),
+            'ST_UNIDAD_LEVANTAMIENTO' : forms.Textarea(attrs={'class': 'form-control', 'rows' : '3'}),
+        }
+
+
+class EstructuraMetalicaForm(forms.ModelForm):
+    class Meta:
+        model = EstructuraMetalica
+        fields = '__all__'
+        exclude = ['SK_ESTRUCTURA_METALICA, FK_PROYECTO']
+        widgets = {
+            'ST_TIPO_DOC_CONCRETO' : forms.Textarea(attrs={'class': 'form-control', 'rows' : '3'}),
+            'ST_DOC_CONCRETO' : forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
+
+class SenializacionVialForm(forms.ModelForm):
+    class Meta:
+        model = SenializacionVial
+        fields = '__all__'
+        exclude = ['SK_SENIALIZACION_VIAL, FK_PROYECTO']
+        widgets = {
+            'ST_ESPECIFICACION_VIAL' : forms.Textarea(attrs={'class': 'form-control', 'rows' : '3'}),
+        }
+
+class AsesoriaConstructivaForm(forms.ModelForm):
+    class Meta:
+        model = AsesoriaConstructiva
+        fields = '__all__'
+        exclude = ['SK_ASESORIA, FK_PROYECTO']
+        widgets = {
+            'NM_TIEMPO' : forms.TextInput(attrs={'class': 'form-control'}),
+            'ST_UNIDAD_ASESORIA' : forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
         }
