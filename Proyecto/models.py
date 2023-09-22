aa7a9ca74241b02f38368a6ea016cf2323c7b354
Kevin Agreda <36802620+KevinAgreda245@users.noreply.github.com>
2023-09-21 23:21:52 -0600

    Incorporación para registro de Asesoria

--- Proyecto/models.py
+++ Proyecto/models.py
@@ -169,8 +169,8 @@ class AsesoriaConstructiva(models.Model):
         ('AÑO','Año')
     ]
     SK_ASESORIA = models.AutoField(primary_key=True)
-    NM_TIEMPO = models.IntegerField(null=False,blank=False)
-    ST_UNIDAD_ASESORIA = models.CharField(max_length=50,choices=UNIDADES)
+    NM_TIEMPO = models.IntegerField(null=False,blank=False,verbose_name="N° de tiempo:")
+    ST_UNIDAD_ASESORIA = models.CharField(max_length=50,choices=UNIDADES,verbose_name="Unidades de tiempo")
     FK_PROYECTO = models.ForeignKey(Proyecto,models.CASCADE)
 
     class Meta:
